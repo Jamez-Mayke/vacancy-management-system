635df5b53846bad0016114fbf0aca8bb81c54534
jamesmayke <jamezmayke28@gmail.com>
2024-09-18 11:20:21 -0300

    criando módulo do candidato e sua entidade

---
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/candidate/entity/CandidateEntity.java
@@ -0,0 +1,67 @@
+package br.com.jamesmayke.vacancy_management_system.modules.candidate.entity;
+
+import java.util.UUID;
+
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+
+    public CandidateEntity() {}
+
+    public CandidateEntity(String name, String username, String email, String password, String description) {
+
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.description = description;
+
+    }
+    
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
