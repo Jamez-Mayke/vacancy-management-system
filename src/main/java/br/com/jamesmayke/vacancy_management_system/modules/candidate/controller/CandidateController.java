428fd35a0e7972212935d7bfb9c20d53610eb9d9
jamesmayke <jamezmayke28@gmail.com>
2024-09-19 23:24:40 -0300

    Adicionando retorno no controller como ResponseEntity

--- src/main/java/br/com/jamesmayke/vacancy_management_system/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/candidate/controller/CandidateController.java
@@ -1,20 +1,29 @@
 package br.com.jamesmayke.vacancy_management_system.modules.candidate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jamesmayke.vacancy_management_system.modules.candidate.dto.CandidateRequest;
-import br.com.jamesmayke.vacancy_management_system.modules.candidate.entity.CandidateEntity;
+import br.com.jamesmayke.vacancy_management_system.modules.candidate.dto.CandidateResponse;
+import br.com.jamesmayke.vacancy_management_system.modules.candidate.useCases.CandidateUseCase;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired CandidateUseCase candidateUseCase;
     
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateRequest payload) {
-        return new CandidateEntity(payload);
+    public ResponseEntity<CandidateResponse> create(@Valid @RequestBody CandidateRequest payload) {
+
+       CandidateResponse candidate = this.candidateUseCase.execute(payload);
+
+       return ResponseEntity.status(HttpStatus.CREATED).body(candidate);
     }
 }
