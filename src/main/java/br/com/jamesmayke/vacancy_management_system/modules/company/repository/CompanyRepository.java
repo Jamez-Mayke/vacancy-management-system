16d1bac5f396994c6dd790b6e59556ee3dcc5b1a
jamesmayke <jamezmayke28@gmail.com>
2024-09-21 15:42:57 -0300

    Criando repository e useCase de company, apensa com um método save

---
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/company/repository/CompanyRepository.java
@@ -0,0 +1,12 @@
+package br.com.jamesmayke.vacancy_management_system.modules.company.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.jamesmayke.vacancy_management_system.modules.company.entity.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    Optional<Company> findByUsernameOrEmail(String username, String email);
+}
---
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/company/useCase/CompanyUseCase.java
@@ -0,0 +1,27 @@
+package br.com.jamesmayke.vacancy_management_system.modules.company.useCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jamesmayke.vacancy_management_system.exceptions.UserFoundException;
+import br.com.jamesmayke.vacancy_management_system.modules.company.entity.Company;
+import br.com.jamesmayke.vacancy_management_system.modules.company.repository.CompanyRepository;
+
+@Service
+public class CompanyUseCase {
+    
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public Company execute(Company payload) {
+        this.companyRepository
+            .findByUsernameOrEmail(payload.getUsername(), payload.getEmail())
+            .ifPresent(
+                company -> {
+                    throw new UserFoundException();
+                }
+            );
+
+           return this.companyRepository.save(payload);
+    }
+}
