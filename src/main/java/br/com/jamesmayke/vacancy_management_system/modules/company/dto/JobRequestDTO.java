4e7de2f29c368ea5e3809a59d2d288715ccf48d4
jamesmayke <jamezmayke28@gmail.com>
2024-10-01 15:38:28 -0300

    Criando DTO para construção da vaga

---
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/company/dto/JobRequestDTO.java
@@ -0,0 +1,9 @@
+package br.com.jamesmayke.vacancy_management_system.modules.company.dto;
+
+public record JobRequestDTO(
+    String level,
+    String description,
+    String benefits
+) {
+    
+}
