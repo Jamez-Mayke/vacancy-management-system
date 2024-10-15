0a1549148c42b4bd60e1521d85b8100a581f2003
jamesmayke <jamezmayke28@gmail.com>
2024-10-15 14:30:22 -0300

    Alterando tipo de retorno do JWT

--- src/main/java/br/com/jamesmayke/vacancy_management_system/providers/JWTProvider.java
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/providers/JWTProvider.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 
 @Service
@@ -14,16 +15,15 @@ public class JWTProvider {
     @Value("${security.token.secret}")
     private String secretKey;
     
-    public String validateToken(String token) {
+    public DecodedJWT validateToken(String token) {
         token = token.replace("Bearer ", "");
         
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         try {
-            var subjectToken = JWT.require(algorithm).build().verify(token).getSubject();
-            return subjectToken;
+            return JWT.require(algorithm).build().verify(token);
         } catch (JWTVerificationException ex) {
-            return "";
+            return null;
         }
         
     }
