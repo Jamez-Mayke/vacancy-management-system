d709c63b10aa6553190ce6f2519ed756e37f3e1e
jamesmayke <jamezmayke28@gmail.com>
2024-10-15 14:27:43 -0300

    Rota get para job

--- src/main/java/br/com/jamesmayke/vacancy_management_system/modules/company/controller/CompanyController.java
+++ src/main/java/br/com/jamesmayke/vacancy_management_system/modules/company/controller/CompanyController.java
@@ -1,8 +1,12 @@
 package br.com.jamesmayke.vacancy_management_system.modules.company.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jamesmayke.vacancy_management_system.modules.company.entity.Company;
 import br.com.jamesmayke.vacancy_management_system.modules.company.useCase.CompanyUseCase;
+import br.com.jamesmayke.vacancy_management_system.modules.company.useCase.JobUseCase;
 
 @RestController
 @RequestMapping("/company")
@@ -17,6 +22,9 @@ public class CompanyController {
 
     @Autowired
     private CompanyUseCase companyUseCase;
+
+    @Autowired
+    private JobUseCase jobUseCase;
     
     @PostMapping("/")
     public ResponseEntity<Object> create(@RequestBody Company payload) {
@@ -26,5 +34,17 @@ public class CompanyController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job/{id}")
+    public ResponseEntity<Object> getJob(@PathVariable UUID id) {
+        
+        try {
+            var result = this.jobUseCase.getJob(id);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(result);
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        
     }
 }
